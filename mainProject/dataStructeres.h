e59aaa4c86a15a8ea8414b70bf1a8120d56be194
KachanVlg <vlad.bend@inbox.ru>
2024-06-21 00:05:23 +0300

    Создан класс Vertex, описывающий вершину графа

--- mainProject/dataStructeres.h
+++ mainProject/dataStructeres.h
@@ -1,4 +1,24 @@
 #ifndef DATASTRUCTERES_H
 #define DATASTRUCTERES_H
+#include <QVector>
+#include <QtCore/QCoreApplication>
+
+
+class Vertex {
+public:
+    int id;
+    QVector<int> approachAdjVertexes;
+    QVector<int> outputAdjVertexes;
+
+    Vertex(QVector<int>approachAdjVertexes, QVector<int> outputAdjVertexes,  int id);
+};
+
+Vertex::Vertex(QVector<int>approachAdjVertexes, QVector<int> outputAdjVertexes,  int id)
+{
+    this->id = id;
+    this->approachAdjVertexes = approachAdjVertexes;
+    this->outputAdjVertexes = outputAdjVertexes;
+}
+
 
 #endif // DATASTRUCTERES_H
